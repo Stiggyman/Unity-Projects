bde6b1adeacd175398e7b69b6566df6cf6bb7d2c
Stiggyman <stig0574@gmail.com>
2022-03-16 11:57:25 +0100

    New Project.
    Just for testing 2D UI

---
+++ UI-Project-2D/Assets/Scripts/ZoomWithMouseWheel.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomWithMouseWheel : MonoBehaviour
+{
+    public float zoomSpeed;
+
+    public float orthographicSizeMin;
+    public float orthographicSizeMax;
+
+    public float fovMin;
+    public float fovMax;
+    private Camera myCamera;
+    void Start()
+    {
+        myCamera = GetComponent<Camera>();
+    }
+
+   
+    void Update()
+    {
+        if (myCamera.orthographic)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                myCamera.orthographicSize += zoomSpeed;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") > 0) ;
+            {
+                myCamera.orthographicSize -= zoomSpeed;
+            }
+            myCamera.orthographicSize = Mathf.Clamp(myCamera.orthographicSize, orthographicSizeMin, orthographicSizeMax);
+
+        }
+        else
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                myCamera.fieldOfView += zoomSpeed;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                myCamera.fieldOfView -= zoomSpeed;
+            }
+            myCamera.fieldOfView = Mathf.Clamp(myCamera.fieldOfView, fovMin, fovMax);
+        }
+    }
+
+}
